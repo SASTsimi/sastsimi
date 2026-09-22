ac0094c2ab5138fb31c9d2ce7e7d17647384e0ff
taehyeon-git <t.j9431@gamil.com>
2026-09-22 10:22:39 +0900

    docs: remove superseded design archives

--- docs/architecture-v5/wiki/serve.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [int]$Port = 8000
-)
-
-$wikiRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
-$architectureRoot = Split-Path -Parent $wikiRoot
-Write-Host "Open http://localhost:$Port/wiki/"
-python -m http.server $Port --directory $architectureRoot
